8b1ec4fc8a3a4e34b5b42fa681fb41a6e6ae80c2
DongLim80 <hyundonglim@gmail.com>
2026-07-22 10:48:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ function renderWeek(dataList) {
   let html = `<table class="week-table">
   <thead>
     <tr>
-      <th width="12%">날짜</th><th width="20%">일정</th><th width="8%">교시</th><th width="15%">수업</th><th width="45%">메모</th>
+      <th width="10%">날짜</th><th width="20%">일정</th><th width="5%">교시</th><th width="10%">수업</th><th width="55%">메모</th>
     </tr>
   </thead>
   <tbody>`;
